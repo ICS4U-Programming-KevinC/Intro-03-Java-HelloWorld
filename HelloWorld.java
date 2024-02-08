b583b650538bfc4cbc20a3d63a296a98c0e7f677
Kevin Csiffary <kevin.csiffary@stu.ocsb.ca>
2024-02-08 02:40:22 +0000

    Created Hello World dialogue script

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+* Shows a simple dialogue to the console.
+*
+* @author  Kevin Csiffary
+* @version 1.0
+* @since   2024-02-07
+*/
+
+// Create class for the script
+public class HelloWorld {
+    // Create main function
+    public static void main(String[] args) {
+        // Display the dialogue to the console
+        System.out.println("Hey Ioana!");
+        System.out.println("How's it going?");
+    }
+}
